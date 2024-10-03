d5174293f0d42dcfa863c35ca0135a2f92a98689
Chrix Eledu <monarchofaether@gmail.com>
2024-10-03 14:55:51 -0400

    Evacuationn warning message in constructor

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,15 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof(sqft) !== 'number') {
-      throw new TypeError('Sqft must be a number')
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must overide evacuationWarningMessage');
+    }
+    if (typeof (sqft) !== 'number') {
+      throw new TypeError('Sqft must be a number');
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
