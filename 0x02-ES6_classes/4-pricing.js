438a6c66dbc12c5f8754eb7bd97a611157e89628
Chrix Eledu <monarchofaether@gmail.com>
2024-10-03 12:53:53 -0400

    New Class Pricing imports Class Currency and implements methods to use attributes in both classes, getter and setter attributes also defined

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,49 @@
+import Currency from './3-currency.js'
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof(amount) !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (!(currency instanceof Currency))
+        throw new TypeError('Currency must be a currency');
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount
+  }
+
+  set amount(newAmount) {
+    if (typeof(newAmount) !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be a currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof(amount) !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof(conversionRate) !== 'number') {
+      throw new TypeError('ConversionRate must be a number');
+    }
+
+    return amount * conversionRate;
+  }
+}
