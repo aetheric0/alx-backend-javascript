99eafc88a1e44129459b1c9ca131b66f507a3012
Chrix Eledu <monarchofaether@gmail.com>
2024-10-03 14:05:01 -0400

    eslint corrected for 4-pricing.js, new abstract class Building file

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,28 +1,29 @@
-import Currency from './3-currency.js'
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    if (typeof(amount) !== 'number') {
+    if (typeof (amount) !== 'number') {
       throw new TypeError('Amount must be a number');
-    } else if (!(currency instanceof Currency))
-        throw new TypeError('Currency must be a currency');
+    } else if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a currency');
+    }
     this._amount = amount;
     this._currency = currency;
   }
 
   get amount() {
-    return this._amount
+    return this._amount;
   }
 
   set amount(newAmount) {
-    if (typeof(newAmount) !== 'number') {
+    if (typeof (newAmount) !== 'number') {
       throw new TypeError('Amount must be a number');
     }
     this._amount = newAmount;
   }
 
   get currency() {
-    return this._currency
+    return this._currency;
   }
 
   set currency(newCurrency) {
@@ -33,14 +34,14 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof(amount) !== 'number') {
+    if (typeof (amount) !== 'number') {
       throw new TypeError('Amount must be a number');
     }
-    if (typeof(conversionRate) !== 'number') {
+    if (typeof (conversionRate) !== 'number') {
       throw new TypeError('ConversionRate must be a number');
     }
 
