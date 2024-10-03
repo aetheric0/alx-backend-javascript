836506f9f9a0a94867eefab2beabca884c3dac98
Chrix Eledu <monarchofaether@gmail.com>
2024-10-03 14:09:44 -0400

    eslinted for 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof(name) !== 'string') {
+    if (typeof (name) !== 'string') {
       throw new TypeError('Name must be a string');
-    } else if (typeof(length) !== 'number') {
+    } else if (typeof (length) !== 'number') {
       throw new TypeError('Length must be a number');
     } else if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array');
@@ -17,7 +17,7 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    if (typeof(newName) !== 'string') {
+    if (typeof (newName) !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = newName;
@@ -28,7 +28,7 @@ export default class HolbertonCourse {
   }
 
   set length(newLength) {
-    if (typeof(newLength) !== 'number') {
+    if (typeof (newLength) !== 'number') {
       throw new TypeError('Students must be an array');
     }
     this._length = newLength;
