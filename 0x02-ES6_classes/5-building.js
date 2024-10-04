039dffbf8af06c6ebe9cb016f7e04cea44bda321
Chrix Eledu <monarchofaether@gmail.com>
2024-10-04 03:34:59 -0400

    a method that overrides evacuationWarningMessage

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,9 +3,6 @@ export default class Building {
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
       throw new Error('Class extending Building must overide evacuationWarningMessage');
     }
-    if (typeof (sqft) !== 'number') {
-      throw new TypeError('Sqft must be a number');
-    }
     this._sqft = sqft;
   }
 
