7ba3bda30839bce81e8b8af00ace1e1f181a3a24
Chrix Eledu <monarchofaether@gmail.com>
2024-10-08 13:48:12 -0400

    Syntax correction: missing function keyword added, correct identifier for map argument corrected

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
-export default updateUniqueItems(MapData) {
-  if (!(map instanceof Map)) {
+export default function updateUniqueItems(MapData) {
+  if (!(MapData instanceof Map)) {
     throw new Error('Cannot process');
   }
 
-  for (const [key, value] of map.entries()) {
+  for (const [key, value] of MapData.entries()) {
     if (value === 1) {
-      map.set(key, 100);
+      MapData.set(key, 100);
     }
   }
 }
