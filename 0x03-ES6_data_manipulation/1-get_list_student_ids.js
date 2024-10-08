1c2c7a02b5d97c8ec09b286d42a31668938352c6
Chrix Eledu <monarchofaether@gmail.com>
2024-10-08 07:16:29 -0400

    Returns the student id of array passed to function

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,9 @@
 export default function getListStudentIds(getListStudents) {
+  if (!Array.isArray(getListStudents)) {
+    return [];
+  }
   const arr = [];
-  getListStudents.forEach((student, index) => {
+  getListStudents.forEach((student) => {
     arr.push(student.id);
   });
 
