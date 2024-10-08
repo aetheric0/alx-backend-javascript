b0ca1a8226bf3d7cf5abfb01f27476b501daa853
Chrix Eledu <monarchofaether@gmail.com>
2024-10-08 12:53:16 -0400

    Returning the DataView of a typed array

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  if (position >= 0 && position < length) {
+    int8Array[position] = value;
+  } else {
+    throw new Error("Position outside range")
+  }
+
+  return new DataView(buffer);
+}
