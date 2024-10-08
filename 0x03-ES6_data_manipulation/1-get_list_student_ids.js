79eee16921d7704d31197dd6edf4bb69f33fa759
Chrix Eledu <monarchofaether@gmail.com>
2024-10-08 07:19:50 -0400

    using the map function

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,10 +2,5 @@ export default function getListStudentIds(getListStudents) {
   if (!Array.isArray(getListStudents)) {
     return [];
   }
-  const arr = [];
-  getListStudents.forEach((student) => {
-    arr.push(student.id);
-  });
-
-  return arr;
+  return getListStudents.map((student) => student.id);
 }
