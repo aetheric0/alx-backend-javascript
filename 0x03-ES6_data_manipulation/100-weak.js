822c9ab6f3aff572c20d37ecb2f5c5a644245e04
Chrix Eledu <monarchofaether@gmail.com>
2024-10-08 13:55:10 -0400

    Using WeakMap to monitor API endpoint

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,18 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (typeof endpoint !== 'object' || endpoint === null) {
+    throw new Error('Invalid endpoint');
+  }
+
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const count = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) {
+    throw new Error("Endpoint load is high");
+  }
+}
