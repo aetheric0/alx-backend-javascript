2bb65a31b28040a8862c22a0e9f47d25a57bf1f9
Mornark <monarchofaether@gmail.com>
2024-11-25 07:04:50 -0500

    Reading from standard input in node

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Holberton School, what is your name?");
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(name) {
+  console.log(`Your name is: ${name.trim()}`);
+  console.log('This important software is now closing');
+  process.exit();
+})
