89b1523598a41efa3954980b445795c761ab8309
Mornark <monarchofaether@gmail.com>
2024-11-26 13:51:47 -0500

    String manipulation of data in database

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,13 +4,15 @@ function countStudents(path) {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n');
     const lineCount = lines.length - 2;
-    console.log("Number of Students: " + lineCount);
-    CSList = lines.filter(n => Boolean(n.find('CS')) === true);
-    console.log(CSList);
-    console.log(lines);
+    console.log('Number of Students: ' + lineCount);
+    CSList = lines.filter(n => n.endsWith('CS'));
+    SWEList = lines.filter(n => n.endsWith('SWE'));
+    CSListCount = CSList.length;
+    SWEListCount = SWEList.length;
+    console.log("Number of students in CS: "  + CSListCount + ". " + "List: " + CSList.map( n => n.split(',')[0]));
+    console.log("Number of students in SWE: " + SWEListCount + ". " + "List: " + SWEList.map(n => n.split(',')[0]));
   } catch(err) {
-    console.log(err);
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 
