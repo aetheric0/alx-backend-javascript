a3952e1848f4f1bf249c182cac38d765e3bf0eda
Mornark <monarchofaether@gmail.com>
2024-11-27 05:07:28 -0500

    ESLint fixed

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,7 +4,7 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
