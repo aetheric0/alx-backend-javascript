b743c2e4a5c84310c06962d5b6709d093b1afd8b
Mornark <monarchofaether@gmail.com>
2024-11-27 04:34:45 -0500

    Using http module instead and setting the Content-Type header

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,10 @@
-const { createServer } = require('node:http');
+const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = createServer((req, res) => {
+const app = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Holberton School!');
 });
 
