74f93775b47cde3f03fbc8bfd61c2843fed61617
Mornark <monarchofaether@gmail.com>
2024-11-27 04:22:35 -0500

    Creating a simple http server with node

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname);
+
+exports.app = app;
