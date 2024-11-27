ea78c3532a35db184aff09b4424bb2d0e608211c
Mornark <monarchofaether@gmail.com>
2024-11-27 03:05:00 -0500

    eslint verified

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,17 +1,18 @@
+const fs = require('fs');
+
 function countStudents(path) {
-  const fs = require('fs');
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n');
     const lineCount = lines.length - 2;
-    console.log('Number of Students: ' + lineCount);
-    CSList = lines.filter(n => n.endsWith('CS'));
-    SWEList = lines.filter(n => n.endsWith('SWE'));
-    CSListCount = CSList.length;
-    SWEListCount = SWEList.length;
-    console.log("Number of students in CS: "  + CSListCount + ". " + "List: " + CSList.map( n => n.split(',')[0]));
-    console.log("Number of students in SWE: " + SWEListCount + ". " + "List: " + SWEList.map(n => n.split(',')[0]));
-  } catch(err) {
+    console.log(`Number of Students: ${lineCount}`);
+    const CSList = lines.filter((n) => n.endsWith('CS'));
+    const SWEList = lines.filter((n) => n.endsWith('SWE'));
+    const CSListCount = CSList.length;
+    const SWEListCount = SWEList.length;
+    console.log(`Number of students in CS: ${CSListCount}. List: ${CSList.map((n) => n.split(',')[0])}`);
+    console.log(`Number of students in SWE: ${SWEListCount}. List: ${SWEList.map((n) => n.split(',')[0])}`);
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
