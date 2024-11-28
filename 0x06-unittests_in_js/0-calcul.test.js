85e7d102ff666b57ad63e04703ba04aa5f67de57
Mornark <monarchofaether@gmail.com>
2024-11-28 01:56:13 -0500

    Inner describe statement removed from test module

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,40 @@
 const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 
-  assert.equal(calculateNumber(5, 6), 11);
-  assert.equal(calculateNumber(5.7, 2.5), 8);
-  assert.equal(calculateNumber(5, 6.3), 11);
-  assert.equal(calculateNumber(6.7, 8), 15);
-  assert.equal(calculateNumber(), NaN);
-  assert.equal(calculateNumber('5', '6'), 56);
-  assert.equal(calculateNumber([1, 3]), NaN);
-  assert.equal(calculateNumber(1.7, 8, 11, 88), 10)
+describe('calculateNumber', function () {
+  it('should return the rounded sum when two whole numbers are passed', function () {
+    assert.equal(calculateNumber(5, 6), 11);
+  });
+});
+
+describe('calculateNumber', function () {
+  it('should return the rounded sum of the numbers passed as the correct whole number', function () {
+    assert.equal(calculateNumber(5.7, 2.5), 8);
+  });
+});
+
+describe('calculateNumber', function () {
+  it('should return the rounded sum of both numbers as the correct whole number', function () {
+    assert.equal(calculateNumber(5, 6.3), 11);
+  });
+});
+
+describe('calculateNumber', function () {
+  it('should return the rounded sum of both numbers as the correct whole number', function () {
+    assert.equal(calculateNumber(6.7, 8), 15);
+  });
+});
+
+describe('calculateNumber', function () {
+  it('should return NaN', function () {
+    assert.equal(calculateNumber(), NaN);
+  });
+});
+
+describe('calculateNumber', function () {
+  describe('More than two arguments passed', function () {
+    it('should return the rounded sum of only the first two arguments', function () {
+      assert.equal(calculateNumber(1.7, 8, 11, 88), 10)
+    });
+  });
+});
