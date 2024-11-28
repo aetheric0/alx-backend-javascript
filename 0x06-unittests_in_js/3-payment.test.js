72089beacd58708790f9cc023ccb4ed78425dd8f
Mornark <monarchofaether@gmail.com>
2024-11-28 04:53:28 -0500

    using .to.be.true to validate spy function

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,17 @@
-const chai = require('chai');
-const expect = chai.expect;
-const sinon = require('sinon');
-const spy = sinon.spy;
+const { expect } = require('chai');
+const { spy } = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment.js');
 const Utils = require('./utils.js');
 
 describe('sendPaymentRequestToApi', () => {
   it('should check if the Utils.calculateNumber function has been called with the same value', () => {
-    let calculateNumberSpy = spy(Utils, 'calculateNumber');
+    const calculateNumberSpy = spy(Utils, 'calculateNumber');
+    const totalAmount = 100;
+    const totalShipping = 20;
 
-    sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20));
+    sendPaymentRequestToApi(totalAmount, totalShipping);
+
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
 
     calculateNumberSpy.restore();
   })
