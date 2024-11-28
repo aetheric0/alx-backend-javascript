171cbcc4d80020e485703e45a27f203c4f0100c3
Mornark <monarchofaether@gmail.com>
2024-11-28 04:57:30 -0500

    using sinon.spy rather than standalone spy

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,18 +1,16 @@
 const { expect } = require('chai');
-const { spy } = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment.js');
-const Utils = require('./utils.js');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
   it('should check if the Utils.calculateNumber function has been called with the same value', () => {
-    const calculateNumberSpy = spy(Utils, 'calculateNumber');
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
     const totalAmount = 100;
     const totalShipping = 20;
 
     sendPaymentRequestToApi(totalAmount, totalShipping);
-
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
-
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
     calculateNumberSpy.restore();
   })
 })
