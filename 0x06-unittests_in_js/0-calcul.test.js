79375b17ca33b20240d932034239ec2227c64c39
Mornark <monarchofaether@gmail.com>
2024-11-28 02:45:58 -0500

    Test case for 0-calcul.js correct format understood

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,65 +2,31 @@ const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 
 describe('calculateNumber', function () {
-  describe('Two Whole Numbers argument', function () {
-    it('should return the rounded sum when two whole numbers are passed', function () {
-      assert.equal(calculateNumber(5, 6), 11);
-    });
+  it('should return the rounded sum when two whole numbers are passed', function () {
+    assert.equal(calculateNumber(5, 6), 11);
   });
-});
 
-describe('calculateNumber', function () {
-  describe('Two floating point Numbers argument', function () {
-    it('should return the rounded sum of the numbers passed as the correct whole number', function () {
-      assert.equal(calculateNumber(5.7, 2.5), 8);
-    });
+  it('should return the rounded sum of the numbers passed as the correct whole number', function () {
+    assert.equal(calculateNumber(5.7, 2.5), 8);
   });
-});
 
-describe('calculateNumber', function () {
-  describe('First argument integer, Second argument floating point', function () {
-    it('should return the rounded sum of both numbers as the correct whole number', function () {
-      assert.equal(calculateNumber(5, 6.3), 11);
-    });
+  it('should return the rounded sum of both numbers as the correct whole number (int and float)', function () {
+    assert.equal(calculateNumber(5, 6.3), 11);
   });
-});
 
-describe('calculateNumber', function () {
-  describe('First argument floating point, Second argument integer', function () {
-    it('should return the rounded sum of both numbers as the correct whole number', function () {
-      assert.equal(calculateNumber(6.7, 8), 15);
-    });
+  it('should return the rounded sum of both numbers as the correct whole number (float and int)', function () {
+    assert.equal(calculateNumber(6.7, 8), 15);
   });
-});
 
-describe('calculateNumber', function () {
-  describe('No arguments passed', function () {
-    it('should return NaN', function () {
-      assert.equal(calculateNumber(), NaN);
-    });
+  it('should return NaN when no arguments are passed', function () {
+    assert.ok(Number.isNaN(calculateNumber()));
   });
-});
 
-describe('calculateNumber', function () {
-  describe('Both arguments passed are Strings', function () {
-    it('should return NaN', function () {
-      assert.equal(calculateNumber('5', '6'), 56);
-    });
+  it('should return NaN when an array of numbers is passed', function () {
+    assert.ok(Number.isNaN(calculateNumber([1, 3])));
   });
-});
 
-describe('calculateNumber', function () {
-  describe('Array of numbers passed', function () {
-    it('should return NaN', function () {
-      assert.equal(calculateNumber([1, 3]), NaN);
-    });
-  });
-});
-
-describe('calculateNumber', function () {
-  describe('More than two arguments passed', function () {
-    it('should return the rounded sum of only the first two arguments', function () {
-      assert.equal(calculateNumber(1.7, 8, 11, 88), 10)
-    });
+  it('should return the rounded sum of only the first two arguments when more than two arguments are passed', function () {
+    assert.equal(calculateNumber(1.7, 8, 11, 88), 10);
   });
 });
