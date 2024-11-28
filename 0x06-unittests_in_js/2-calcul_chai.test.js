f5c094a3174e7597bb24bfdada32394fae6f9df4
Mornark <monarchofaether@gmail.com>
2024-11-28 03:37:44 -0500

    Three additional test cases added for negative numbers

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -7,9 +7,17 @@ describe('calculateNumber', function () {
     expect(calculateNumber('SUM', 5, 6)).to.equal(11);
   });
 
+  it('Testing when type is SUM for two negative integers', () => {
+    expect(calculateNumber('SUM', -4, -5)).to.equal(-9);
+  })
+
   it('should return the difference of two numbers when type is SUBTRACT', function () {
     expect(calculateNumber('SUBTRACT', 5.7, 2.5)).to.equal(3);
   });
+
+  it('Testing when type is SUBTRACT with two negative integers', () => {
+    expect(calculateNumber('SUBTRACT', -5, -9)).to.equal(4);
+  })
 
   it('should return the quotient of two numbers when type is DIVIDE', function () {
     expect(calculateNumber('DIVIDE', 8, 2)).to.equal(4);
@@ -18,6 +26,10 @@ describe('calculateNumber', function () {
   it('should return "Error" when dividing by 0', function () {
     expect(calculateNumber('DIVIDE', 6.7, 0)).to.equal('Error');
   });
+
+  it('Testing when type is DIVIDE with two negative integers', () => {
+    expect(calculateNumber('DIVIDE', -4, -2)).to.equal(2);
+  })
 
   it('return should be of type String', function () {
     expect(calculateNumber('DIVIDE', 5, 0)).to.be.a('string');
